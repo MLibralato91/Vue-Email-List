c3aa671980c37e70a710180a0363ef00ec9a415c
MLibralato91 <michelelibralato911@gmail.com>
2023-04-11 14:55:05 +0200

    new Project with API

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      titolo: 'esperimento API',
+      rndEmail: [],
+      basePath: 'https://flynn.boolean.careers/exercises/api/'
+    }
+  },
+  methods: {
+    getData() {
+      this.rndEmail = [];
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.basePath + 'random/mail').then((res) =>{
+          this.rndEmail.push(res.data.response);
+          console.log(this.rndEmail);
+        })
+      }
+      
+    }
+  },
+  mounted() {
+    this.getData();
+  },
+}).mount('#app');
+
